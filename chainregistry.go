89524d1f77bf69fecb0eeec0a83a5c4f4562fb50
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-10-12 10:15:59 -0700

    use chain configs from sparkswap fork to update ltc genesis hash

--- chainregistry.go
+++ chainregistry.go
@@ -316,19 +316,8 @@ func newChainControlFromConfig(cfg *config, chanDB *channeldb.DB,
 
 		// Establish the connection to bitcoind and create the clients
 		// required for our relevant subsystems.
-		if homeChainConfig.Node == "litecoind" {
-			fmt.Printf("What in the.....")
-			fmt.Printf("%v\n", bitcoinMainNetParams.Params.GenesisHash)
-			fmt.Printf("%v\n", bitcoinTestNetParams.Params.GenesisHash)
-			fmt.Printf("%v\n", regTestNetParams.Params.GenesisHash)
-			fmt.Printf("%v\n", bitcoinSimNetParams.Params.GenesisHash)
-			fmt.Printf("Going back to litecoin")
-			fmt.Printf("%v\n", litecoinMainNetParams.Params.GenesisHash)
-			fmt.Printf("%v\n", litecoinTestNetParams.Params.GenesisHash)
-			fmt.Printf("%v\n", litecoinRegTestNetParams.Params.GenesisHash)
-			activeNetParams.Params.GenesisHash = regTestNetParams.Params.GenesisHash
-			activeNetParams.Params.GenesisBlock = regTestNetParams.Params.GenesisBlock
-		}
+		// if homeChainConfig.Node == "litecoind" {
+		// }
 
 		bitcoindConn, err := chain.NewBitcoindConn(
 			activeNetParams.Params, bitcoindHost,
